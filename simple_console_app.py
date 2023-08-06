4274a19897332d0e21f7cd466ef0772aa6a82701
John-M <johnmaryluxin8@gmail.com>
2023-08-06 17:12:20 +0300

    wrote first console app

---
+++ simple_console_app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+import cmd
+
+
+class HelloWorld(cmd.Cmd):
+    """Simple command processor"""
+
+    FRIENDS = ["Alice", "Adam", "Barbara", "Bob"]
+
+    # define the first command
+    def do_greet(self, person):
+        """greet [person]
+        Greet the named person"""
+
+        greeting = "Hello"
+        if person and person in self.FRIENDS:
+            greeting = "Hi, %s!" % person
+        elif person:
+            print("Hello, " + person)
+            return
+
+        print(greeting)
+
+    # format live help
+    def help_greet(self):
+        print(
+            "\n".join(
+                [
+                    "greet [person]",
+                    "Greet the named person",
+                ]
+            )
+        )
+
+    # built-in auto completion
+    def complete_greet(self, text, line, begidx, endidx):
+        if not text:
+            completions = self.FRIENDS[:]
+        else:
+            completions = [f for f in self.FRIENDS if f.startswith(text)]
+        return completions
+
+    # define method to exit the application when done
+    def do_EOF(self, line):
+        return True
+
+
+# run application only if called by name
+if __name__ == "__main__":
+    HelloWorld().cmdloop()
